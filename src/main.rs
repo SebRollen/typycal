1692fa89e43186d82d0a58dd895334431641cd8d
Sebastian Rollen <seb@rollen.io>
2022-05-19 17:42:23 -0400

    read arguments from command line for num_words and num_peek

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use structopt::StructOpt;
 use typycal::Game;
 
+#[derive(Debug, StructOpt)]
+struct App {
+    #[structopt(default_value = "10")]
+    words: usize,
+    #[structopt(short, long, default_value = "3")]
+    num_peek: u16,
+}
+
 fn main() -> std::io::Result<()> {
-    let game = Game::words(10).num_peek(3);
+    let app = App::from_args();
+    let game = Game::words(app.words).num_peek(app.num_peek);
     game.play()
 }
